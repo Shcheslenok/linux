96d44bac69d876e7af3f07c4684b16aba033a04a
Peter <sheslenok@bk.ru>
2018-04-17 02:43:02 +0300

    fixed mistakes

--- tema1/task2/minskweather.sh
+++ tema1/task2/minskweather.sh
@@ -19,12 +19,9 @@ N=$(sed 's/N=//' config.ini)
 
 while true
 do
-	wget -O pogoda_minsk -o wget_log https://pogoda.tut.by
+	wget -O /tmp/pogoda_minsk -o /tmp/wget_log https://pogoda.tut.by
 
-	temp_now=$(grep 'class="temp-i"' pogoda_minsk |
-	head -n 1 |
-	sed 's/<span class="temp-i">//; s!&deg;</span></div>!!')
+	echo "Now: $(grep 'class="temp-i"' "/tmp/pogoda_minsk" | head -n 1 | sed 's/<span class="temp-i">//; s!&deg;</span></div>!!')"
 
-	echo "Now: $temp_now"
 	sleep $N
 done
